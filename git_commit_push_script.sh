1c56d57265148e0273ef6e0272627d94abe2741a
Tommy Wilbur <wilburtw@amazon.com>
2021-04-16 10:49:31 -0700

    All other file changes up to 2021_04_16

---
+++ git_commit_push_script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd Desktop/AWSCalc/
+day_val=$(date +'%Y_%m_%d')
+git status
+git add SeleniumOutputs/
+git commit -m "Selenium Crawls gathered up to ${day_val}"
+git add AutomatedSummary.txt
+git commit -m "AutomatedSummary.txt changes up to ${day_val}"
+git add *
+git commit -m "All other file changes up to ${day_val}"
