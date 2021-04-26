09bb2b0e793fad2b1b3047e7c071b2ec7008e15f
Tommy Wilbur <wilburtw@amazon.com>
2021-04-26 09:04:37 -0700

    Added README.md commands to concatenate it with AutomatedSummary.txt

--- git_commit_push_script.sh
+++ git_commit_push_script.sh
@@ -6,6 +6,10 @@ git add SeleniumOutputs/
 git commit -m "Selenium Crawls gathered up to ${day_val}"
 git add AutomatedSummary.txt
 git commit -m "AutomatedSummary.txt changes up to ${day_val}"
+cp text_README.md README.md
+cat AutomatedSummary.txt >> README.md
+git add README.md
+git commit -m "README.md changes up to ${day_val}"
 git add *
 git commit -m "All other file changes up to ${day_val}"
 git push
