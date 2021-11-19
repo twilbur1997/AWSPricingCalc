52c7323f2cbfb34758f52bef5f3bfee274e045a5
Tommy_EC2_ubuntu <wilburtw@amazon.com>
2021-11-19 23:08:20 +0000

    Made two git push scripts for linux vs Mac.

---
+++ git_linux.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+/usr/bin/python3 wilburtw/AWSPricingCalc/AWSCalculatorNewServices.py 
+/usr/bin/python3 wilburtw/AWSPricingCalc/SummarizeServicesAdded.py
+cd wilburtw/AWSPricingCalc
+day_val=$(date +'%Y_%m_%d')
+git status
+git add SeleniumOutputs/
+git commit -m "Selenium Crawls gathered up to ${day_val}"
+git add AutomatedSummary.txt
+git commit -m "AutomatedSummary.txt changes up to ${day_val}"
+cp text_README.md README.md
+cat AutomatedSummary.txt >> README.md
+git add README.md
+git commit -m "README.md changes up to ${day_val}"
+git add *
+git commit -m "All other file changes up to ${day_val}"
+git push
+git status
